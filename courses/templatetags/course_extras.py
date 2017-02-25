5cade53cd14fc67136c74eb9696ea1420ca3cf6f
vitaliimyshliaev <mishlaev@gmail.com>
2017-02-25 14:42:06 +0300

    Custom filter 'time_estimate'

--- courses/templatetags/course_extras.py
+++ courses/templatetags/course_extras.py
@@ -8,3 +8,16 @@ register = template.Library()
 def newest_course():
     ''' Gets the most recent course that was added to the library. '''
     return Course.objects.latest('created_at')
+
+
+@register.inclusion_tag('courses/course_nav.html')
+def nav_courses_list():
+    '''Returns dictionary of course to display as nav panel'''
+    courses = Course.objects.all()
+    return {'courses': courses}
+
+@register.filter('time_estimate')
+def time_estimate(word_count):
+    '''Estimates the number of minutes to compleate a step based on the passed-in wordcount'''
+    minutes = round(word_count/20)
+    return minutes
