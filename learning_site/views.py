e55f14488003da7b74f0a2cbd6a15f313a74f053
vitaliimyshliaev <mishlaev@gmail.com>
2017-03-01 17:26:48 +0300

    Custom validators

--- learning_site/views.py
+++ learning_site/views.py
@@ -1,5 +1,12 @@
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
+
+from .forms import SuggestionForm
+from . import forms
 
 def hello_world(request):
     return render(request, 'home.html')
@@ -12,3 +19,20 @@ def my_404_view(request):
         context_instance=RequestContext(request))
     response.status_code = 404
     return response
+
+
+def suggestion_view(request):
+    form = forms.SuggestionForm()
+    if request.method == 'POST':
+        form = forms.SuggestionForm(request.POST)
+        if form.is_valid():
+            send_mail(
+                'Suggestion from {}'.format(form.cleaned_data['name']),
+                form.cleaned_data['suggestion'],
+                '{name} <{email}>'.format(**form.cleaned_data),
+                ['mishlaev@gmail.com']
+            )
+            messages.add_message(request, messages.SUCCESS,
+                                 'Thanks for your suggestion!')
+            return HttpResponseRedirect(reverse('suggestion'))
+    return render(request, 'suggestion_form.html', {'form': form})
