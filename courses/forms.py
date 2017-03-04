72d0b750f2027efec68992ea4f219af88a26bac6
vitaliimyshliaev <mishlaev@gmail.com>
2017-03-04 23:58:14 +0300

    Answer formset

--- courses/forms.py
+++ courses/forms.py
@@ -44,3 +44,10 @@ class AnswerForm(forms.ModelForm):
             'text',
             'correct'
         ]
+
+
+AnswerFormSet = forms.modelformset_factory(
+    models.Answer,
+    form=AnswerForm,
+    extra=2,
+)
--- courses/views.py
+++ courses/views.py
@@ -117,15 +117,17 @@ def quiz_question(request, quiz_pk, question_pk):
 def answer_form(request, question_pk):
     question = get_object_or_404(models.Question, pk=question_pk)
 
-    form = forms.AnswerForm()
+    formset = forms.AnswerFormSet(queryset=question.answer_set.all())
 
     if request.method == "POST":
-        form = forms.AnswerForm(request.POST)
-        if form.is_valid():
-            answer = form.save(commit=False)
-            answer.question = question
-            answer.save()
-            messages.success(request, 'Answer Added!')
-            return HttpResponseRedirect(question.get_absolute_url())
+        formset = forms.AnswerFormSet(request.POST,
+                                      queryset=question.answer_set.all())
+        if formset.is_valid():
+            answers = formset.save(commit=False)
+            for answer in answers:
+                answer.question = question
+                answer.save()
+            messages.success(request, "Added answers!")
+            return HttpResponseRedirect(question.quiz.get_absolute_url())
     return render(request, "courses/answer_form.html", {'question': question,
-                                                        'form': form})
+                                                        'formset': formset})
